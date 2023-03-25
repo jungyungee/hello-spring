ee30a1b6e0a6103e5af1435cd2d888896e2e8d28
yungee <jungyungee0610@gmail.com>
2023-03-26 03:06:45 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+} //웹 어플리케이션에서 /hello 라고 들어오면 해당 메소드를 호출해줌
